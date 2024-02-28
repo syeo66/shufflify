5aff129c9a769538609b8a04acbc98384323ac10
Red Ochsenbein <red@redochsenbein.ch>
2024-02-28 18:30:53 +0100

    Update permissions

--- src/functions/signInWithSpotify.ts
+++ src/functions/signInWithSpotify.ts
@@ -2,8 +2,7 @@ function signInWithSpotify() {
   const { VITE_CLIENT_ID: CLIENT_ID } = import.meta.env
 
   const appUrl = encodeURIComponent(window.location.href.split('#')[0])
-  const scopes =
-    'user-library-read playlist-read-private playlist-modify-private user-modify-playback-state user-read-playback-state'
+  const scopes = 'user-library-read playlist-read-private playlist-modify-private playlist-modify-public'
   const url =
     `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&redirect_uri=${appUrl}&response_type=token` +
     `&scope=${encodeURIComponent(scopes)}`
