9e614252b1b5163091597d793d13a9936e51bf56
Red Ochsenbein <red@redochsenbein.ch>
2024-05-18 19:46:53 +0200

    remove unnecessary async from stats store

--- src/stores/stats.ts
+++ src/stores/stats.ts
@@ -12,21 +12,15 @@ interface StoreData {
 
 const INITIAL_STATE = { selectedTrackCount: 0, selectedPlaylists: [] }
 
-export const stats = derived<Stores, StoreData>(
-  [configuration, playlists, savedTrackCount],
-  async ([$c, $p, $s], set) => {
-    if ($p.isLoading) {
-      set(INITIAL_STATE)
-    }
+export const stats = derived<Stores, StoreData>([configuration, playlists, savedTrackCount], ([$c, $p, $s], set) => {
+  if ($p.isLoading) {
+    set(INITIAL_STATE)
+  }
 
-    const selectedPlaylists = $p.data?.filter((playlist) => $c.checkedPlaylists.includes(playlist.id)) ?? []
+  const selectedPlaylists = $p.data?.filter((playlist) => $c.checkedPlaylists.includes(playlist.id)) ?? []
 
-    const selectedTrackCount =
-      ($c.syncFavorites ? $s : 0) +
-      (selectedPlaylists.reduce((acc, playlist) => {
-        return acc + playlist.tracks.total
-      }, 0) ?? 0)
+  const selectedTrackCount =
+    ($c.syncFavorites ? $s : 0) + (selectedPlaylists.reduce((acc, playlist) => acc + playlist.tracks.total, 0) ?? 0)
 
-    set({ selectedTrackCount, selectedPlaylists })
-  },
-)
+  set({ selectedTrackCount, selectedPlaylists })
+})
